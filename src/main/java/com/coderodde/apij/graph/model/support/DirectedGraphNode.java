f9b90847804b1076a8f411fbc9c615fc374a9c28
Rodion Efremov <rodion.efremov@helsinki.fi>
2014-05-22 10:31:27 +0300

    Added directed graph node, lacks unit testing.

---
+++ src/main/java/com/coderodde/apij/graph/model/support/DirectedGraphNode.java
@@ -0,0 +1,252 @@
+package com.coderodde.apij.graph.model.support;
+
+import com.coderodde.apij.graph.model.Graph;
+import com.coderodde.apij.graph.model.Node;
+import static com.coderodde.apij.util.Utils.checkBelongsToGraph;
+import static com.coderodde.apij.util.Utils.checkNotNull;
+import static com.coderodde.apij.util.Utils.checkSameGraphs;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ *
+ * @author rodionefremov
+ */
+public class DirectedGraphNode extends Node<DirectedGraphNode> {
+
+    /**
+     * The name must be unique within a holder graph as it defines a node's
+     * identity.
+     */
+    private final String name;
+    
+    /**
+     * An adjacency list for incoming arcs.
+     */
+    private final Set<DirectedGraphNode> in;
+    
+    /**
+     * An adjacency list for outgoing arcs.
+     */
+    private final Set<DirectedGraphNode> out;
+    
+    /**
+     * Constructs a new <code>UndirectedGraphNode</code> with name 
+     * <code>name</code>.
+     * 
+     * @param name the name of a new node.
+     */
+    public DirectedGraphNode(final String name) {
+        checkNotNull(name, "The name of a node may not be 'null'.");
+        this.name = name;
+        this.in =  new HashSet<>();
+        this.out = new HashSet<>();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterable<DirectedGraphNode> parentIterable() {
+        return new Iterable<DirectedGraphNode>() {
+            @Override
+            public Iterator<DirectedGraphNode> iterator() {
+                return new ParentIterator();
+            }
+        };
+    }
+
+    /**
+     * Returns an <code>Iterator</code> over this node's children.
+     * 
+     * @return an <code>Iterator</code> over this node's children. 
+     */
+    @Override
+    public Iterator<DirectedGraphNode> iterator() {
+        return new ChildIterator();
+    }
+
+    /**
+     * Connects this node to <code>child</code>.
+     * 
+     * @param child the edge target node.
+     */
+    @Override
+    public void connectTo(final DirectedGraphNode child) {
+        checkNotNull(child, "'child' is 'null'.");
+        checkBelongsToGraph(this);
+        checkBelongsToGraph(child);
+        checkSameGraphs(this.getOwnerGraph(), child.getOwnerGraph());
+        
+        if (isConnectedTo(child)) {
+            return;
+        }
+        
+        this.out.add(child);
+        child.in.add(this);
+        incEdgeCount();
+    }
+
+    /**
+     * Disconnect <code>child</code> and <code>this</code>.
+     * 
+     * @param child the node to disconnect from.
+     */
+    @Override
+    public void disconnect(DirectedGraphNode child) {
+        checkNotNull(child, "'child' is 'null'.");
+        checkBelongsToGraph(this);
+        checkBelongsToGraph(child);
+        checkSameGraphs(this.getOwnerGraph(), child.getOwnerGraph());
+        
+        if (this.isConnectedTo(child)) {
+            this.out.remove(child);
+            child.in.remove(this);
+            decEdgeCount();
+        }
+    }
+
+    @Override
+    public boolean isConnectedTo(DirectedGraphNode child) {
+        return out.contains(child);
+    }
+    
+    /**
+     * Returns the reference to the owner graph or <code>null</code> if there is
+     * no such.
+     * 
+     * @return the owner graph.
+     */
+    @Override
+    public Graph<DirectedGraphNode> getOwnerGraph() {
+        return this.ownerGraph;
+    }
+
+    /**
+     * Sets the owner graph for this node.
+     * 
+     * @param graph the graph to set as an owner.
+     */
+    @Override
+    public void setOwnerGraph(Graph<DirectedGraphNode> graph) {
+        if (this.getOwnerGraph() != null) {
+            this.clear();
+            this.getOwnerGraph().removeNode(this);
+        }
+        
+        this.ownerGraph = graph;
+    }
+    
+    @Override
+    public void clear() {
+        final Iterator<DirectedGraphNode> iterator = iterator();
+        
+        while (iterator.hasNext()) {
+            iterator.next();
+            iterator.remove();
+            this.decEdgeCount();
+        }
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof DirectedGraphNode)) {
+            return false;
+        }
+        
+        return ((DirectedGraphNode) o).getName().equals(this.getName());
+    }
+    
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+    
+    /**
+     * This inner class implements the iterator over enclosing's node's 
+     * children.
+     */
+    private class ChildIterator implements Iterator<DirectedGraphNode> {
+
+        /**
+         * The actual iterator, no need to reinvent a wheel.
+         */
+        private Iterator<DirectedGraphNode> iterator = 
+                DirectedGraphNode.this.out.iterator();
+        
+        /**
+         * Caches the node returned by <code>next()</code> most recently.
+         */
+        private DirectedGraphNode lastReturned;
+        
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public DirectedGraphNode next() {
+            return (lastReturned = iterator.next());
+        }
+        
+        @Override
+        public void remove() {
+            if (lastReturned == null) {
+                throw new NoSuchElementException("There is no current node.");
+            }
+            
+            iterator.remove();
+            lastReturned.in.remove(DirectedGraphNode.this);
+            lastReturned = null;
+        }
+    }
+    
+    /**
+     * This inner class implements the iterator over enclosing's node's 
+     * children.
+     */
+    private class ParentIterator implements Iterator<DirectedGraphNode> {
+
+        /**
+         * The actual iterator, no need to reinvent a wheel.
+         */
+        private Iterator<DirectedGraphNode> iterator = 
+                DirectedGraphNode.this.in.iterator();
+        
+        /**
+         * Caches the node returned by <code>next()</code> most recently.
+         */
+        private DirectedGraphNode lastReturned;
+        
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public DirectedGraphNode next() {
+            return (lastReturned = iterator.next());
+        }
+        
+        @Override
+        public void remove() {
+            if (lastReturned == null) {
+                throw new NoSuchElementException("There is no current node.");
+            }
+            
+            iterator.remove();
+            lastReturned.out.remove(DirectedGraphNode.this);
+            lastReturned = null;
+        }
+    }
+}
--- src/main/java/com/coderodde/apij/graph/path/afs/support/kdTree.java
+++ src/main/java/com/coderodde/apij/graph/path/afs/support/kdTree.java
@@ -2,14 +2,33 @@ package com.coderodde.apij.graph.path.afs.support;
 
 import com.coderodde.apij.graph.model.Node;
 import com.coderodde.apij.graph.path.Layout;
+import com.coderodde.apij.graph.path.afs.Partitioner;
 import static com.coderodde.apij.util.Utils.checkNotNull;
 import static com.coderodde.apij.util.Utils.checkPositive;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 
-public class kdTree<T extends Node<T>> {
+public class kdTree<T extends Node<T>> implements Partitioner<T> {
+    
+    private static final boolean X = false;
+    private static final boolean Y = true;
+    
+//    private static Comparator<Point2D.Double> xcmp = new XComparator();
+//    
+//    private static Comparator<Point2D.Double> ycmp = new YComparator();
+    
+    private boolean axis = X;
     
     private int maximumNodesPerPartition;
     
     private Layout<T> layout;
+    
+    private Object[] nodes;
+    
+    private List<Integer> separatorList;
     
     public kdTree(final int maximumNodesPerPartition,
                   final Layout<T> layout) {
@@ -17,5 +36,41 @@ public class kdTree<T extends Node<T>> {
                       "'maximumNodesPerPartition' is less than 1.");
         checkNotNull(layout, "'layout' is null.");
         this.maximumNodesPerPartition = maximumNodesPerPartition;
+        this.separatorList = new ArrayList<>();
+    }
+
+    @Override
+    public List<Set<T>> partition(Set<T> nodes) {
+        this.separatorList.clear();
+        this.axis = X;
+        this.nodes = new Object[nodes.size()];
+        List<Set<T>> partition = new ArrayList<>();
+        
+        int i = 0;
+        
+        for (final T node : nodes) {
+            this.nodes[i++] = node;
+        }
+        
+        
+        return partition;
+    }
+    
+    
+//    private class XComparator implements Comparator<> {
+//        
+//        private Layout<T> layout;
+//        
+////        @Override
+////        public int compare(Point2D.Double o1, Point2D.Double o2) {
+////            return (o1.x < o2.x ? -1 : (o1.x > o2.x ? 1 : 0));
+////        }
+//    }
+    
+    private static class YComparator implements Comparator<Point2D.Double> {
+        @Override
+        public int compare(Point2D.Double o1, Point2D.Double o2) {
+            return (o1.y < o2.y ? - 1 : (o1.y > o2.y ? 1 : 0));
+        }
     }
 }
